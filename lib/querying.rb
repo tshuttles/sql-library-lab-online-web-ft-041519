34f4319b7e2a5feebe7e9d39e552cbe385a58599
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-23 16:13:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
   FROM characters 
   JOIN series 
   ON series.id = characters.series_id
-  GROUP BY series.title"
+  GROUP BY series.title, "
 end
 
 def select_character_names_and_number_of_books_they_are_in
