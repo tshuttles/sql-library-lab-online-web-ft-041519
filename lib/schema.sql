2306add4f903ab01761dc2a1015145962fb64406
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-23 15:25:56 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,12 +2,12 @@ CREATE TABLE series (
 id INTEGER PRIMARY KEY,
 title TEXT,
 author_id INTEGER,
-subgenre_id INTEGER,
+subgenre_id INTEGER
 );
 
 CREATE TABLE subgenres (
 id INTEGER PRIMARY KEY,
-name TEXT,
+name TEXT
 );
 
 CREATE TABLE authors (
