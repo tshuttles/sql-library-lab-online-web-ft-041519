cdb752ad868c8cc90af65af0f53c438d34524017
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-23 15:06:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,18 +1,28 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books WHERE books.series_id = 1 ORDER BY books.year"
+  "SELECT books.title, books.year 
+  FROM books 
+  WHERE books.series_id = 1 
+  ORDER BY books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters ORDER BY characters.motto ASC LIMIT 1"
+  "SELECT characters.name, characters.motto 
+  FROM characters 
+  ORDER BY characters.motto ASC LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) FROM characters GROUP BY characters.species ORDER BY COUNT(characters.species) DESC LIMIT 1"
+  "SELECT characters.species, COUNT(characters.species) 
+  FROM characters 
+  GROUP BY characters.species 
+  ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors "
+  "SELECT authors.name, subgenres.name 
+  FROM authors 
+  JOIN "
 end
 
 def select_series_title_with_most_human_characters
