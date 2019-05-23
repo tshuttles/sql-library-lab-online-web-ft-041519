d6ce0cfd9bafb24b0f3bfbdbae758b039bb2d9c1
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-23 15:06:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name 
   FROM authors 
   JOIN series
-  ON"
+  ON authors.id "
 end
 
 def select_series_title_with_most_human_characters
