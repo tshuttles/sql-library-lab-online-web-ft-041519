7d644427e390ffcf5f5162671695ce732a7015dc
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-23 16:16:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors JOIN series ON authors.id = series.author_id JOIN subgenres ON series.subgenre_id = subgenres.id
+  "SELECT authors.name, subgenres.name 
+  FROM authors 
+  JOIN series ON authors.id = series.author_id 
+  JOIN subgenres ON series.subgenre_id = subgenres.id
   "
 end
 
